2a4ccdac7aa31b3bb76606bc332798a8bdc42254
Ravindu Piyankara <78452012+Ravindu-Priyankara@users.noreply.github.com>
2023-01-11 14:28:08 +0530

    Add Admin panel to my Max Cinema Project

---
+++ src/main/java/com/RavinduPriyankaraPremachandra/maxcinemaadminproject/MaxCinemaAdminProjectApplication.java
@@ -0,0 +1,15 @@
+package com.RavinduPriyankaraPremachandra.maxcinemaadminproject;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class MaxCinemaAdminProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaxCinemaAdminProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/RavinduPriyankaraPremachandra/maxcinemaadminproject/MaxCinemaAdminProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.RavinduPriyankaraPremachandra.maxcinemaadminproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaxCinemaAdminProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
